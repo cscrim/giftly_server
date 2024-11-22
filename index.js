9d82684ad3b40458a4f0dd7eef90721cbbf0abd0
Courtney S <courtneyscrimshaw@gmail.com>
2024-11-22 17:02:51 -0500

    include wishlistRouter to index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import "dotenv/config";
 import cors from 'cors';
+import wishlistRouter from './routes/wishlist_routes.js';
 
 const { PORT, BACKEND_URL, CORS_ORIGIN } = process.env;
 
@@ -8,6 +9,8 @@ const app = express()
 
 app.use(cors({ origin: CORS_ORIGIN }));
 app.use(express.json());
+
+app.use("/", wishlistRouter);
 
 app.listen(PORT, () => {
     console.log(`The server is listening on ${BACKEND_URL}:${PORT}`);
