7c12a73aa4bf2bee05e5895f950e1204c02027b9
Courtney S <courtneyscrimshaw@gmail.com>
2024-12-15 17:56:24 -0500

    cleaning up some code

--- controllers/userController.js
+++ controllers/userController.js
@@ -62,48 +62,13 @@ const addItem = async (req, res) => {
         purchase_link,
         description,
       });
-    //   }).returning("*");
-  
-      // respond with the item
+ 
       res.status(201).json(newItem);
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: "Failed to add item to wishlist" });
     }
   };
-
-
-//   const editItem = async (req, res) => {
-//     const { item_name, item_img, price, purchase_link, description } = req.body;
-//     const { item_id } = req.params;  
-  
-
-//     const updateFields = {};
-//     if (item_name) updateFields.item_name = item_name;
-//     if (item_img) updateFields.item_img = item_img;
-//     if (price) updateFields.price = price;
-//     if (purchase_link) updateFields.purchase_link = purchase_link;
-//     if (description) updateFields.description = description;
-//     updateFields.updated_at = knex.fn.now();  
-    
-//     try {
-      
-//       const [updatedItem] = await knex("wishlist_items")
-//         .where({ item_id })  
-//         .update(updateFields) 
-//         // .returning("*"); 
-  
-//       if (!updatedItem) {
-//         return res.status(404).json({ error: "Item not found." });
-//       }
-  
-//       // respond with the updated item
-//       res.status(200).json(updatedItem);
-//     } catch (error) {
-//       console.error(error);
-//       res.status(500).json({ error: "Failed to update item." });
-//     }
-//   };
 
 
 const editItem = async (req, res) => {
